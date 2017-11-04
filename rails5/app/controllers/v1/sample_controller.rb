1c32dc4e575eb3e3b130636e71a9c609e067401f
yamashiro.ryota <yamashiro0110@gmail.com>
2017-11-04 09:18:41 +0900

    mod sample controller

--- rails5/app/controllers/v1/sample_controller.rb
+++ rails5/app/controllers/v1/sample_controller.rb
@@ -1,11 +1,11 @@
-class V1::SampleController < ApplicationController
+module V1
+  class SampleController < ApplicationController
+    def index
+      render 'index', formats: 'json', handlers: 'jbuilder'
+    end
 
-  def index
-    render 'index', formats: 'json', handlers: 'jbuilder'
+    def redis
+      render 'redis', formats: 'json', handlers: 'jbuilder'
+    end
   end
-
-  def redis
-    render 'redis', formats: 'json', handlers: 'jbuilder'
-  end
-
 end
