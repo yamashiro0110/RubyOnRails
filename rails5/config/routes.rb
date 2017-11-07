b204bc318691df27de2e521f055e30c91b906a6d
yamashiro.ryota <yamashiro0110@gmail.com>
2017-11-08 00:17:58 +0900

    edit route config

--- rails5/config/routes.rb
+++ rails5/config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   ['v1.0'].each {|version|
     scope version do
-      get 'sample',       to: 'v1/sample#index'
-      get 'sample/redis', to: 'v1/sample#redis'
+      get  'sample/get',    to: 'v1/sample#index'
+      post 'sample/post',   to: 'v1/sample#post'
+      post 'access_token',  to: 'v1/sample#access_token'
     end
   }
 
