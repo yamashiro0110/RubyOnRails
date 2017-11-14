ca3a3d358c07af2ff22e4b3223b57bbea2557988
yamashiro.ryota <yamashiro0110@gmail.com>
2017-11-15 00:59:17 +0900

    mod sample controller

--- rails5/app/controllers/v1/sample_controller.rb
+++ rails5/app/controllers/v1/sample_controller.rb
@@ -1,20 +1,15 @@
-module V1
-  class SampleController < ApplicationController
-    def index
-      render 'index', formats: 'json', handlers: 'jbuilder'
-    end
-
-    def post
-      @sample_model = SampleModel.create(msg: params[:msg], path: params[:path], method: params[:method])
-      render 'post', formats: 'json', handlers: 'jbuilder', status: @sample_model.status
-    end
-
-    def access_token
-      user_id = params[:user_id]
-      @access_token = AccessToken.create(user_id)
-      @access_token.save!
-      render 'access_token', formats: 'json', handlers: 'jbuilder'
-    end
-
+class V1::SampleController < ApplicationController
+  def index
+    render 'index', formats: 'json', handlers: 'jbuilder'
   end
+
+  def post
+    @sample_model = SampleModel.create(msg: params[:msg], path: params[:path], method: params[:method])
+    render 'post', formats: 'json', handlers: 'jbuilder', status: @sample_model.status
+  end
+
+  def access_token
+    render 'access_token', formats: 'json', handlers: 'jbuilder'
+  end
+
 end
